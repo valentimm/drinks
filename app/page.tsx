258b51f0b8260d89cd215a1cdf61690e2431c114
Matheus Valentim <meduardo.valentim@gmail.com>
2025-06-24 23:28:31 -0300

    fix: correct sizes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Dices, Martini } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="w-5/6 h-[48rem] mx-auto my-10 bg-card1 rounded-2xl shadow-lg flex items-center justify-center">
+    <div className="w-5/6 sm:h-[48rem] h-auto mx-auto mt-4 bg-card1 rounded-2xl shadow-lg flex items-center justify-center">
       <div className="mt-4 flex flex-col items-center w-full h-full p-8 gap-4 rounded-lg">
         <div className="flex items-center justify-center w-full gap-2">
           <div className="flex flex-col items-center w-full gap-4 sm:flex-row">
@@ -24,7 +24,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="px-18 flex flex-wrap gap-4 items-center justify-between w-full h-full border-accent border-2">
+        <div className="sm:px-18 flex sm:flex-wrap flex-col sm:gap-4 items-center gap-4 w-full h-full border-accent border-2">
           <div className="rounded-lg sm:w-[31rem] sm:h-[31rem] overflow-hidden shadow-lg">
             <img
               src="https://www.thecocktaildb.com//images/media/drink/vrwquq1478252802.jpg/medium"
@@ -33,10 +33,7 @@ export default function Home() {
             />
           </div>
           <div className="sm:w-[46rem] sm:h-[31rem] bg-card2 rounded-lg p-8 px-12 shadow-lg flex flex-col items-center gap-4">
-            <span className="text-3xl font-bold text-text">Nome do Drink</span>
-            <span className="text-lg text-text-secondary">Categoria: Cocktail</span>
-            <span className="text-lg text-text-secondary">Álcool: Sim</span>
-            <span className="text-lg text-text-secondary">Ingredientes:</span>
+            <span className="sm:text-3xl font-bold text-text">Nome do Drink</span>
             <ul className="list-disc list-inside text-text-secondary">
               <li>Ingrediente 1</li>
               <li>Ingrediente 2</li>
